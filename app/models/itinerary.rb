1ebae3589567926b86e230b09e46a1dddc09075d
catnamedboo <64523208+germainewongg@users.noreply.github.com>
2022-06-08 22:29:40 +0800

    created the model associations

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,4 +1,8 @@
 class Itinerary < ApplicationRecord
   belongs_to :organiser
   belongs_to :chatroom
+  has_many :events
+  has_many :users, through: :itinerary_users
+  has_many :criteria, through: :itinerary_criterium
+  has_many :announcements
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :notifications
+  has_many :messages
+  has_many :user_chatrooms
+  has_many :itinerary_users
 end
