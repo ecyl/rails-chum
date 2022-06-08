f0286a3fc59e1e05139eb9cb49dc524e0f18d4ad
catnamedboo <64523208+germainewongg@users.noreply.github.com>
2022-06-08 23:01:03 +0800

    Validations for the events model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,11 @@
 class Event < ApplicationRecord
   belongs_to :itinerary
+
+  # Validations
+  validates :title, presence: true
+  validates :date_start, presence: true
+  validates :date_end, presence: true
+  validates :location, presence: true
+  validates :cost, numericality: { only_integer: true, greater_than: 0 }
+  validates :description, presence: true
 end
