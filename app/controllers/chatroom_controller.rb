9bb9a4677a821a6c51d5fafe186344edc127c7c0
catnamedboo <64523208+germainewongg@users.noreply.github.com>
2022-06-08 16:14:06 +0800

    created controllers and some of the methods for the various controllers

---
+++ app/controllers/chatroom_controller.rb
@@ -0,0 +1,7 @@
+class ChatroomController < ApplicationController
+  def show
+  end
+
+  def create
+  end
+end
---
+++ app/controllers/notifications_controller.rb
@@ -0,0 +1,7 @@
+class NotificationsController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+end
