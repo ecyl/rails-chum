e5903f3272eb1c9be65e4483996c43a4765cdb0a
raymond ong <raymondong.nus@gmail.com>
2022-06-08 16:15:23 +0800

    halfway thru seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Cleaning database..."
+User.destroy_all
+Itinerary.destroy_all
+Event.destroy_all
+ItineraryUser.destroy_all
+ItineraryCriterium.destroy_all
+Criterium.destroy_all
+Announcement.destroy_all
+Notification.destroy_all
+Chatroom.destroy_all
+Message.destroy_all
+UserChatroom.destroy_all
+
+puts "Creating users..."
+
+# 10 users
+10.times do
+  user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    age: [1..80].sample
+    gender: ["M","F"].sample,
+    email: Faker::Internet.email
+  )
+end
+
+
+# 30 events
+
+# 10 itineraries
