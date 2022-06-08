892fc82f5ee034459c86f8548975982e44e83509
catnamedboo <64523208+germainewongg@users.noreply.github.com>
2022-06-08 22:53:15 +0800

    Added validations for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ApplicationRecord
   has_many :messages
   has_many :user_chatrooms
   has_many :itinerary_users
+
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :age, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :gender, presence: true
 end
