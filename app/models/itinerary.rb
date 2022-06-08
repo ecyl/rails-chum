83972772a10a37a61f8cf6c1d08a518288553ec4
raymond ong <raymondong.nus@gmail.com>
2022-06-08 18:41:46 +0800

    improve seed

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,4 +1,4 @@
 class Itinerary < ApplicationRecord
-  belongs_to :organiser
+  belongs_to :user
   belongs_to :chatroom
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -23,27 +23,64 @@ puts "Creating users..."
     last_name: Faker::Name.last_name,
     age: (1..80).to_a.sample,
     gender: ["M", "F"].sample,
-    email: Faker::Internet.email
+    email: Faker::Internet.email,
+    password: "12345678"
   )
   user.save!
 end
 
 users = User.all
 
+
+
 puts "Creating events..."
 
-# 30 events
-30.times do
-  random_start_date = Faker::Date.between(from '2022-05-20', to: '2022-12-28')
-  random_end_date = random_start_date + (3..14).to_a.sample
-  puts random_end_date
-  binding.pry
-  event = Event.new(
-    description: Faker::Company.catch_phrase,
-    date_start: random_start_date,
-    date_end: random_end_date,
-    location: Faker::TvShows::Simpsons.location,
-  )
-end
+LOCATION_STRING = ["10 Bayfront Ave, Singapore 018956", "313 Somerset Road",
+  "53 Ang Mo Kio Avenue 3", "Queen Street", "Seletar Mall", "Tampines Mall",
+  "Changi City Point", "Jewel Airport", "National University of Singapore",
+  "Orchard Gateway", "Nex", "Vivo City", "Bukchon Hanok Traditional Village",
+  "National Museum of Korea", "Lotte World Tower", "Gyeongbokgung Palace",
+  "Bukhansan National Park", "Eiffel Tower", "Louvre Museum", "Palace of Versailles", "Mont Blanc", "Dune du Pilat", "Parc National des Calanques"]
+
+
+  # 30 events
 
 # 10 itineraries
+puts "Creating itineraries..."
+10.times do
+
+  # create chatroom that comes with the itinerary
+  chatroom = Chatroom.new(
+    name: Faker::Lorem.word
+  )
+
+  chatroom.save!
+
+  random_start_date = Faker::Date.between(from: '2022-05-20', to: '2022-12-28')
+  random_end_date = random_start_date + (3..14).to_a.sample
+  itinerary = Itinerary.new(
+    title: "A great time in #{Faker::Nation.capital_city}",
+    participant_limit: (2..8).to_a.sample,
+    description: Faker::Company.catch_phrase,
+    deadline: random_start_date - (3..10).to_a.sample
+  )
+
+  itinerary.chatroom = chatroom
+  itinerary.user = User.all.sample
+  itinerary.save!
+
+
+  3.times do
+    event = Event.new(
+      title: Faker::Mountain.name,
+      description: Faker::Company.catch_phrase,
+      date_start: random_start_date,
+      date_end: random_end_date,
+      location: LOCATION_STRING.sample,
+      cost: (50..1000).to_a.sample
+    )
+    event.itinerary = itinerary
+    event.save!
+
+  end
+end
