53cf1dc7fdbce36f0f3ca372b0c1147a96d8d9db
catnamedboo <64523208+germainewongg@users.noreply.github.com>
2022-06-08 17:29:24 +0800

    Edited the devise controller and forms to accept firstname lastname etc

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,10 +1,18 @@
 class ItinerariesController < ApplicationController
   def index
+    @itineraries = Itinerary.all
   end
 
   def new
   end
 
   def create
+  end
+
+  private
+
+  def itinerary_params
+    params.require(:itinerary).permit(:title, :participant_limit, :description,
+                                      :deadline, :finalised, :user_id, :chatroom_id)
   end
 end
