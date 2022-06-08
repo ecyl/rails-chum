cc4eb41764e021b5b5b8a9518248a81f225c2932
raymond ong <raymondong.nus@gmail.com>
2022-06-08 17:10:03 +0800

    seeds improvement

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 require 'faker'
+require 'pry-byebug'
 
 puts "Cleaning database..."
 User.destroy_all
@@ -20,13 +21,29 @@ puts "Creating users..."
   user = User.new(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
-    age: [1..80].sample
-    gender: ["M","F"].sample,
+    age: (1..80).to_a.sample,
+    gender: ["M", "F"].sample,
     email: Faker::Internet.email
   )
+  user.save!
 end
 
+users = User.all
+
+puts "Creating events..."
 
 # 30 events
+30.times do
+  random_start_date = Faker::Date.between(from '2022-05-20', to: '2022-12-28')
+  random_end_date = random_start_date + (3..14).to_a.sample
+  puts random_end_date
+  binding.pry
+  event = Event.new(
+    description: Faker::Company.catch_phrase,
+    date_start: random_start_date,
+    date_end: random_end_date,
+    location: Faker::TvShows::Simpsons.location,
+  )
+end
 
 # 10 itineraries
