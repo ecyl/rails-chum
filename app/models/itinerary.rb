bb792b4952d87f5d63354940667e6d52b41780ea
catnamedboo <64523208+germainewongg@users.noreply.github.com>
2022-06-08 22:58:15 +0800

    Added validations to the itinerary model

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -5,4 +5,13 @@ class Itinerary < ApplicationRecord
   has_many :users, through: :itinerary_users
   has_many :criteria, through: :itinerary_criterium
   has_many :announcements
+
+  # Validations
+  validates :title, presence: true
+  validates :location, presence: true
+  validates :date_start, presence: true
+  validates :date_end, presence: true
+  validates :cost, numericality: { only_integer: true, greater_than: 0 }
+  validates :deadline, presence: true
+  validates :chatroom_id, presence: true
 end
