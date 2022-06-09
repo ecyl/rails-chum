e351910fa894f2120613e6a8d70cbc3ca16e594d
raymond ong <raymondong.nus@gmail.com>
2022-06-09 14:43:31 +0800

    created itinerary form

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -4,15 +4,25 @@ class ItinerariesController < ApplicationController
   end
 
   def new
+    @itinerary = Itinerary.new
   end
 
   def create
+    @itinerary.Itinerary.new(itinerary_params)
+    @itinerary.user = current_user
+    @itinerary.chatroom = Chatroom.new
+
+    if @itinerary.save
+      redirect_to itinerary_path(@itinerary)
+    else
+      render :new
+    end
   end
 
   private
 
   def itinerary_params
     params.require(:itinerary).permit(:title, :participant_limit, :description,
-                                      :deadline, :finalised, :user_id, :chatroom_id)
+                                      :deadline, :finalised)
   end
 end
