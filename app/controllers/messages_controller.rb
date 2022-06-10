3cfbf0799aba29ee96ac31b732c5d6c6b7ccae49
eunice chin <ecyl2461@gmail.com>
2022-06-10 14:06:47 +0800

    added some style to the chatroom

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,7 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     @message.chatroom = @chatroom
     @message.user = current_user
+
     if @message.save
       redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
     else
