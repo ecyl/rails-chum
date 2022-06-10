f66e5b933de9f96db801ed75c8f3824faf64553a
eunice chin <ecyl2461@gmail.com>
2022-06-10 13:26:58 +0800

    created chatroom policy for pundit

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
     # #confirm method
   end
 
-  resources :chatroom, only: :show do
+  resources :chatrooms, only: :show do
     resources :messages, only: :create
   end
 
