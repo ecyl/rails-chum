7090472f6e0044e7e153d97666de376b41d775e6
eunice chin <ecyl2461@gmail.com>
2022-06-11 09:42:40 +0800

    added index method to display all the chats

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,7 +1,10 @@
 class ChatroomsController < ApplicationController
+  def index
+    @chatrooms = policy_scope(Chatroom).where(id: current_user.chatroom_ids)
+  end
+
   def show
-    @chatroom = Chatroom.find(params[:id])
-    authorize @chatroom
+    @chatroom = authorize Chatroom.find(params[:id])
     @message = Message.new
   end
 end
