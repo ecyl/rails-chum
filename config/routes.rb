af85a4350b3eb01dd9d666dfa1f57238698ea26a
eunice chin <ecyl2461@gmail.com>
2022-06-11 12:25:02 +0800

    created a route for new chatroom

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :chatrooms, only: [ :index, :show, :create ] do
+  resources :chatrooms, only: [ :index, :show, :new, :create ] do
     resources :messages, only: :create
   end
 
