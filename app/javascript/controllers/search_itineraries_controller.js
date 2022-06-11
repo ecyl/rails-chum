60521eb212982a43e64ae24541d62637c06903d2
helen tan <helentanmz0909@gmail.com>
2022-06-11 23:04:07 +0800

    Added Event listener with data action

--- app/javascript/controllers/search_itineraries_controller.js
+++ app/javascript/controllers/search_itineraries_controller.js
@@ -4,9 +4,13 @@ export default class extends Controller {
   static targets = ["form", "input", "list"];
 
   connect() {
-    console.log("search itineraries controller is now connected");
-    console.log(this.formTarget);
-    console.log(this.inputTarget);
-    console.log(this.listTarget);
+    // console.log("search itineraries controller is now connected");
+    // console.log(this.formTarget);
+    // console.log(this.inputTarget);
+    // console.log(this.listTarget);
+  }
+
+  update(e) {
+    console.log("An ajax request will be here");
   }
 }
