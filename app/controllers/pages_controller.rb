841e892f40d08094b88f7e25b388b59f19840baa
helen tan <helentanmz0909@gmail.com>
2022-06-11 17:34:17 +0800

    order itinerary cards by latest created_at date

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @itineraries = Itinerary.all
+    @itineraries = Itinerary.order(created_at: :desc)
   end
 end
