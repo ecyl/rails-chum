eb43aa1d1cb34d3fad0dddd208078b225fbc69f5
helen tan <helentanmz0909@gmail.com>
2022-06-11 22:59:49 +0800

    Selected elements using data target

--- app/javascript/controllers/search_itineraries_controller.js
+++ app/javascript/controllers/search_itineraries_controller.js
@@ -1,9 +1,12 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  static targets = [];
+  static targets = ["form", "input", "list"];
 
   connect() {
     console.log("search itineraries controller is now connected");
+    console.log(this.formTarget);
+    console.log(this.inputTarget);
+    console.log(this.listTarget);
   }
 }
