400d643f9077920fe0acc72b00886254d34acfe0
eunice chin <ecyl2461@gmail.com>
2022-06-14 17:39:02 +0800

    able to toggle between group chats and private chats

---
+++ app/javascript/controllers/chat_sidebar_controller.js
@@ -0,0 +1,31 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["tabOne", "tabTwo", "tab", "tabHeader"]
+
+  connect() {
+    console.log("hello from chat sidebar")
+  }
+
+  toggleTabs(event) {
+    event.preventDefault();
+    console.log(this.tabTargets)
+    // when a tab is selected, hide the other tab
+    const tabs = this.tabTargets;
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    })
+
+    const tabHeaders = this.tabHeaderTargets;
+    tabHeaders.forEach((tabHeader) => {
+      tabHeader.classList.remove("active");
+    })
+
+    event.currentTarget.classList.add("active");
+
+    const currentTab = this[event.currentTarget.dataset.tab];
+    currentTab.classList.add("active");
+
+  }
+
+}
