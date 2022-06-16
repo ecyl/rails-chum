812795a4ea786cef4cfc921acf18d17958f7a569
helen tan <helentanmz0909@gmail.com>
2022-06-16 14:17:19 +0800

    ok flatpickr options are not working

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,7 +1,10 @@
 import flatpickr from "flatpickr";
+import 'flatpickr/dist/themes/airbnb.css'
 
 const initFlatpickr = () => {
-  flatpickr(".datepicker", {});
+  flatpickr(".datepicker", {
+    altInput: true
+  });
 }
 
 export { initFlatpickr };
