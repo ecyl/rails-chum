0bcb382814a8c7077f93a4a64618b24344464d6f
raymond ong <raymondong.nus@gmail.com>
2022-06-16 13:24:28 +0800

    shifted the badge to beside avatar on navbar

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,6 +3,16 @@ class NotificationsController < ApplicationController
   def index
     # @notifications = Notification.where(user_id: current_user.id).order(created_at: :desc)
     @notifications = policy_scope(Notification).where(user_id: current_user.id).order(created_at: :desc)
+    @notifications.each do |notification|
+      mark_as_read!(notification)
+      notification.save
+    end
     authorize @notifications
+  end
+
+  private
+
+  def mark_as_read!(notification)
+    notification.read = true
   end
 end
