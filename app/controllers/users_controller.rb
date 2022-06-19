00b585b0f819178eae2f5d60e80e6a671c731227
catnamedboo <64523208+germainewongg@users.noreply.github.com>
2022-06-19 16:16:28 +0800

    Refactored reviews controller and fixed weird break

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,14 @@ class UsersController < ApplicationController
   def show
     @user = authorize User.find(params[:id])
     @static_navbar = true
+
+    @reviews = []
+    @user.itineraries.each do |itinerary|
+      if !itinerary.reviews.length.zero?
+        itinerary.reviews.each do |review|
+          @reviews << review
+        end
+      end
+    end
   end
 end
