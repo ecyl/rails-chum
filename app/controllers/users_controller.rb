6116b1c4b3748ec022e168dceb056b87a5823349
catnamedboo <64523208+germainewongg@users.noreply.github.com>
2022-06-19 17:11:00 +0800

    refactored controller logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,16 @@ class UsersController < ApplicationController
     @static_navbar = true
 
     @reviews = []
+    @organised_trips = []
     @user.itineraries.each do |itinerary|
       if !itinerary.reviews.length.zero?
         itinerary.reviews.each do |review|
           @reviews << review
         end
+      end
+
+      if itinerary.itinerary_users.where(status: "organiser").include?(@user)
+        @oragnised_trips << itinerary
       end
     end
   end
